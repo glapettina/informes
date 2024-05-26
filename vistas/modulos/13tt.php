9068aa02d0b5a8b34d048e200191c16eb3ace262
glapettina <guillermo@glsoftware.com.ar>
2024-05-26 19:30:17 -0300

    Corrección Físico Química

--- vistas/modulos/13tt.php
+++ vistas/modulos/13tt.php
@@ -283,7 +283,7 @@
 
                     } 
                     
-                    if ($mat == 'Físico - Química') {
+                    if ($mat == 'Físico Química') {
                     
                       $aulico = 'aulicoFisicoQuimica';
                       $comportamiento = 'comportamientoFisicoQuimica';
